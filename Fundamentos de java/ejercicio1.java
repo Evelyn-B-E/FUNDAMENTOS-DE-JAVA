1855ec3cd73030af2f7d7785c7f3caeffa0087bf
Evelyn Beltrán Espinoza <67941068+Evelyn-B-E@users.noreply.github.com>
2020-08-09 23:13:57 -0500

    Add files via upload

---
+++ Fundamentos de java/ejercicio1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class NumeroParImpar {
+	
+	public static void main(String[] args) {
+		
+		int numero;
+		
+		System.out.println(¨Leer número:¨);
+		Scanner=
+	}
+}
---
+++ Fundamentos de java/encontrar.java
@@ -0,0 +1,12 @@
+public class NumeroMayor {
+	public static void main(String[] arg) {
+		int n1 = 10;
+		int n2 = 11;
+		int n3 = 12;
+		
+		String mensaje = "";
+		if(n1 > n2 && n1 > n3) { 
+			mensaje = n1  + "E"
+		}
+		}
+}
